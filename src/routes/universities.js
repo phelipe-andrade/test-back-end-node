eeb38f8f60fd66c5122f929b0b0c9b9524e15e81
Phelipe <88689679+phelipe-andrade@users.noreply.github.com>
2022-07-08 12:54:44 -0300

    Update universities.js

--- src/routes/universities.js
+++ src/routes/universities.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
-//const login = require('../middleware/login');
 const universitiesControllers = require('../controllers/universitiesControllers');
 
-// RETORNA TODOS OS PRODUTOS
 router.get('/', universitiesControllers.getUniversities);
 router.get('/:id', universitiesControllers.getUniversitiesById);
 router.post('/', universitiesControllers.postUniversity);
